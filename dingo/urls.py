4f509bc22129c7c36b3ff8ac433831e1fd017011
Jacek Bakuński <jacek.bakunski@gmail.com>
2023-01-08 13:11:16 +0100

    Add greetings App

--- dingo/urls.py
+++ dingo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('maths/', include('maths.urls')),
+    path('greetings/', include('greetings.urls')),
 ]
